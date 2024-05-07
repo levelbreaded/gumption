b782b6dd42bf49f45ee01fe82fc8dceae3f89004
Opey Adeyemi <60050089+Opeyem1a@users.noreply.github.com>
2024-05-07 16:47:27 -0600

    allow hop command to take a search term (#15)
    * allow hop command to take a search term
    
    * give feedback when search yields nothing
    
    * update only if current branch hasn't been fetched
    
    * prevent refetching current branch
    
    * remove explicit app exit
    
    * lint
    
    * allow showing the current branch in select list if it fits the search terms
    
    * add IIFE
    
    * update searchTerm variable naming
    
    * lint fix

--- src/commands/hop.tsx
+++ src/commands/hop.tsx
@@ -1,52 +1,69 @@
 import ErrorDisplay from '../components/error-display.js';
 import GumptionItemComponent from '../components/gumption-item-component.js';
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import SelectInput from 'ink-select-input';
-import { Box, Text, useApp } from 'ink';
+import { Box, Text } from 'ink';
+import { CommandProps } from '../command-registry.js';
 import { useGit } from '../hooks/use-git.js';
 
-function Hop() {
+function Hop({ input }: CommandProps) {
     const git = useGit();
-    const { exit } = useApp();
     const [allBranches, setAllBranches] = useState<string[]>([]);
-    const [currentBranch, setCurrentBranch] = useState('');
+    const [currentBranch, setCurrentBranch] = useState<string | undefined>(
+        undefined
+    );
     const [newBranch, setNewBranch] = useState<string | undefined>(undefined);
     const [error, setError] = useState<Error | undefined>(undefined);
 
+    const [searchTerm]: Array<string | undefined> = input;
+
     useEffect(() => {
-        const getLocalBranches = async () => {
-            const { all, current } = await git.branchLocal();
-            setAllBranches(all);
-            setCurrentBranch(current);
-        };
-
-        void getLocalBranches();
-    }, [git, setAllBranches, setCurrentBranch]);
-
-    const handleSelect = (item: { label: string; value: string }) => {
-        const updateCurrentBranch = async () => {
+        if (currentBranch) return;
+        void (async () => {
             const { current } = await git.branchLocal();
-            setNewBranch(current);
-        };
+            setCurrentBranch((prev) => prev ?? current);
+        })();
+    }, [git, setCurrentBranch, currentBranch]);
 
-        git.checkout(item.value)
-            .then(async () => {
-                return updateCurrentBranch();
-            })
-            .catch((error: Error) => {
-                setError(error);
-            })
-            .finally(() => {
-                exit();
-            });
-    };
+    useEffect(() => {
+        void (async () => {
+            const { all } = await git.branchLocal();
+            setAllBranches(all);
+        })();
+    }, [git, setAllBranches]);
+
+    const handleSelect = useCallback(
+        (item: { label: string; value: string }) => {
+            const updateCurrentBranch = async () => {
+                const { current } = await git.branchLocal();
+                setNewBranch(current);
+            };
+
+            git.checkout(item.value)
+                .then(() => {
+                    void updateCurrentBranch();
+                })
+                .catch((error: Error) => {
+                    setError(error);
+                });
+        },
+        [git, setNewBranch, setError]
+    );
 
     const items = useMemo(() => {
+        if (!currentBranch) return [];
+
         if (allBranches.length === 1) {
             return [{ label: currentBranch, value: currentBranch }];
         }
+
         return allBranches
-            .filter((branch) => branch !== currentBranch)
+            .filter((branch) =>
+                searchTerm
+                    ? branch.toLowerCase().includes(searchTerm.toLowerCase())
+                    : true
+            )
+            .filter((branch) => searchTerm || branch !== currentBranch)
             .map((branch) => ({
                 label: branch,
                 value: branch,
@@ -57,22 +74,37 @@ function Hop() {
         return <ErrorDisplay error={error} />;
     }
 
-    return newBranch ? (
-        <Box flexDirection="column">
-            <Box gap={1}>
-                <Text dimColor italic color="cyan">
-                    {currentBranch}
-                </Text>
-                <Text bold>↴</Text>
-            </Box>
+    if (!items.length && searchTerm) {
+        return (
             <Text>
-                Hopped to{' '}
-                <Text bold color="green">
-                    {newBranch}
+                No branches match the pattern:{' '}
+                <Text color="cyan" bold>
+                    {searchTerm}
                 </Text>
             </Text>
-        </Box>
-    ) : (
+        );
+    }
+
+    if (newBranch) {
+        return (
+            <Box flexDirection="column">
+                <Box gap={1}>
+                    <Text dimColor italic color="cyan">
+                        {currentBranch}
+                    </Text>
+                    <Text bold>↴</Text>
+                </Box>
+                <Text>
+                    Hopped to{' '}
+                    <Text bold color="green">
+                        {newBranch}
+                    </Text>
+                </Text>
+            </Box>
+        );
+    }
+
+    return (
         <SelectInput
             items={items}
             itemComponent={GumptionItemComponent}
@@ -84,7 +116,7 @@ function Hop() {
 
 export const hopConfig = {
     description: 'Hop to other branches',
-    usage: 'hop',
+    usage: 'hop | hop <SEARCH_TERM>',
 };
 
 export default Hop;
