3e5aae15bdbfd14fabe2ca4857452dd603170a62
opeyem1a <opeyadeyemi@gmail.com>
2024-10-20 03:29:18 -0600

    vertical ls with colors

--- src/commands/list.tsx
+++ src/commands/list.tsx
@@ -1,57 +1,52 @@
 import React, { useMemo } from 'react';
 import { Box, Text } from 'ink';
 import { CommandConfig } from '../types.js';
+import { ForegroundColorName } from 'chalk';
+import { LiteralUnion } from 'type-fest';
 import { Loading } from '../components/loading.js';
 import { SelectRootBranch } from '../components/select-root-branch.js';
 import { treeToParentChildRecord } from '../utils/tree-helpers.js';
 import { useGitHelpers } from '../hooks/use-git-helpers.js';
 import { useTree } from '../hooks/use-tree.js';
 
-const colorMap = ['blue', 'red', 'green', 'yellow', 'magenta', 'cyan', 'white'];
+interface TextStyle {
+    color: LiteralUnion<ForegroundColorName, string>;
+    dimColor?: boolean;
+}
 
-const BranchTreeDisplay = ({
-    treeParentChildRecord,
-    displayedBranchName,
-    currentBranch,
-    prefix = '',
-    isLast = true,
-    depth = 0,
-}: {
-    treeParentChildRecord: Record<string, string[]>;
-    displayedBranchName: string;
-    currentBranch: string;
-    prefix?: string;
-    isLast?: boolean;
-    depth?: number;
-}) => {
-    // Misc UNICODE for reference: └ ─ ┘ ┌ ┐ ├ | ● ○
-    const connector = depth === 0 ? '   ' : isLast ? '└──' : '├──';
-    const color = colorMap[depth % colorMap.length];
-    const updatedPrefix = `${prefix}${isLast ? '   ' : '|  '}`;
-    const children = treeParentChildRecord?.[displayedBranchName] ?? [];
+const styleMap: TextStyle[] = [
+    { color: 'cyan' },
+    { color: 'blue' },
+    { color: 'yellow' },
+    { color: 'magentaBright' },
+    { color: 'green' },
+    { color: 'blueBright' },
+    { color: 'yellowBright' },
+    { color: 'magenta' },
+    { color: 'cyanBright' },
+];
 
+const Connectors = ({ connectors }: { connectors: DisplayElement[] }) => {
     return (
         <>
-            <Text key={displayedBranchName}>
-                {`${prefix}${connector}${currentBranch === displayedBranchName ? '◉' : '○'} `}
-                <Text color={color}>{`${displayedBranchName}\n`}</Text>
-            </Text>
-            {children.map((child) => {
+            {connectors.map((connector, index) => {
+                const style = styleMap[index % styleMap.length] as TextStyle;
                 return (
-                    <React.Fragment key={child}>
-                        <BranchTreeDisplay
-                            treeParentChildRecord={treeParentChildRecord}
-                            displayedBranchName={child}
-                            currentBranch={currentBranch}
-                            prefix={updatedPrefix}
-                            isLast={child === children[children.length - 1]}
-                            depth={depth + 1}
-                        />
-                    </React.Fragment>
+                    <Text
+                        key={`connector-${index}`}
+                        color={style.color}
+                        dimColor={style.dimColor}
+                    >
+                        {connector.symbols}
+                    </Text>
                 );
             })}
         </>
     );
+};
+
+const Spaces = ({ count }: { count: number }) => {
+    return <>{Array(count).fill('  ')}</>;
 };
 
 export const List = () => {
@@ -70,17 +65,150 @@ export const List = () => {
         return <Loading />;
     }
 
+    const nodes = getDisplayNodes({
+        record: treeParentChildRecord,
+        branchName: rootBranchName,
+    });
+    const maxWidth = maxWidthFromDisplayNodes({ displayNodes: nodes });
+
     return (
-        <Box flexDirection="column">
-            <Text>
-                <BranchTreeDisplay
-                    treeParentChildRecord={treeParentChildRecord}
-                    displayedBranchName={rootBranchName}
-                    currentBranch={currentBranch.value}
-                />
-            </Text>
+        <Box flexDirection="column" gap={0}>
+            {nodes.map((node) => {
+                const style = styleMap[
+                    node.prefix.length % styleMap.length
+                ] as TextStyle;
+                return (
+                    <Text key={node.name}>
+                        <Connectors
+                            connectors={[
+                                ...node.prefix,
+                                {
+                                    symbols:
+                                        currentBranch.value === node.name
+                                            ? '◉'
+                                            : '◯',
+                                },
+                                ...node.suffix,
+                            ]}
+                        />
+                        <Spaces count={maxWidth + 2 - node.width} />
+                        <Text color={style.color} dimColor={style.dimColor}>
+                            {node.name}
+                        </Text>
+                    </Text>
+                );
+            })}
         </Box>
     );
+};
+
+type DisplayElement = {
+    symbols: string;
+};
+
+type DisplayNode = {
+    prefix: DisplayElement[];
+    suffix: DisplayElement[];
+    name: string;
+    /**
+     * How "wide" (in increments of 2 spaces) the row will be. This helps to line up the branch names on the right
+     */
+    width: number;
+};
+
+const getDisplayNodes = ({
+    record,
+    branchName,
+    childIndex = 0,
+    parentPrefix = [],
+    parentWidth = 0,
+    depth = 0,
+}: {
+    record: Record<string, string[]>;
+    branchName: string;
+    childIndex?: number;
+    parentPrefix?: DisplayElement[];
+    parentWidth?: number;
+    depth?: number;
+}): DisplayNode[] => {
+    const prefix = [...parentPrefix, ...prefixFromChildIndex({ childIndex })];
+    const children = record[branchName] ?? [];
+    const suffix = suffixFromNumChildren({
+        numChildren: children.length,
+    });
+
+    const widthWithoutChildren = (parentWidth ?? 0) + (childIndex ?? 0);
+    const widthFromChildren = children.length > 1 ? children.length - 1 : 0;
+
+    let nodes: DisplayNode[] = [];
+    children.forEach((childBranch, index) => {
+        const childNodes = getDisplayNodes({
+            record,
+            branchName: childBranch,
+            childIndex: index,
+            parentPrefix: prefix,
+            parentWidth: widthWithoutChildren,
+            depth: depth + 1,
+        });
+        nodes = [...nodes, ...childNodes];
+    });
+
+    nodes = [
+        ...nodes,
+        {
+            prefix,
+            suffix,
+            name: branchName,
+            width: widthWithoutChildren + widthFromChildren,
+        },
+    ];
+
+    return nodes;
+};
+
+const prefixFromChildIndex = ({
+    childIndex,
+}: {
+    childIndex: number;
+}): DisplayElement[] => {
+    return Array(childIndex)
+        .fill(null)
+        .map(() => {
+            return {
+                symbols: '│ ',
+            };
+        });
+};
+
+const suffixFromNumChildren = ({
+    numChildren,
+}: {
+    numChildren: number;
+}): DisplayElement[] => {
+    if (numChildren < 1) return [] as DisplayElement[];
+
+    const length = numChildren - 1;
+    return Array(length)
+        .fill(null)
+        .map((_, index) => {
+            return {
+                symbols: index === length - 1 ? '─┘' : '─┴',
+            };
+        });
+};
+
+const maxWidthFromDisplayNodes = ({
+    displayNodes,
+}: {
+    displayNodes: DisplayNode[];
+}) => {
+    let maxWidth = 0;
+
+    displayNodes.forEach((node) => {
+        maxWidth = Math.max(node.width, maxWidth);
+    });
+
+    return maxWidth;
 };
 
 export const listConfig: CommandConfig = {
